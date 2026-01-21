b128bfbbf0f9d7d2f262490be0dadfee943908ba
Blast <oshri040406@gmail.com>
2026-01-21 10:17:02 +0200

    - some locilization keys for bosses

--- Quests/VanillaQuests/Bosses/KingSlimeDefeated.cs
+++ Quests/VanillaQuests/Bosses/KingSlimeDefeated.cs
@@ -1,9 +1,17 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Localization;
 
 namespace QuestBooks.Quests.VanillaQuests.Bosses
 {
     public class KingSlimeDefeated : Quest
     {
         public override bool CheckCompletion() => NPC.downedSlimeKing;
+        public override void MakeSimpleInfoPage(out string title, out string contents, out Texture2D texture)
+        {
+            title = Language.GetTextValue("Mods.QuestBooks.Questbooks.Bosses.KingSlime.Title");
+            contents = Language.GetTextValue("Mods.QuestBooks.QuestBooks.Bosses.KingSlime.Contents");
+            texture = null;
+        }
     }
 }
