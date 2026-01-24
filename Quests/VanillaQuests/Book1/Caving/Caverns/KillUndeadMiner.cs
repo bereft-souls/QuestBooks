470f39a70f00801c10602318d91fc7ced686c9f9
Blast <oshri040406@gmail.com>
2026-01-24 14:03:26 +0200

    - Rest of the 1.2 skeleton files

---
+++ Quests/VanillaQuests/Book1/Caving/Caverns/KillUndeadMiner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestBooks.Quests.VanillaQuests.Book1.Caving.Caverns
+{
+    internal class KillUndeadMiner : Quest
+    {
+        public override bool CheckCompletion()
+        {
+            return false;
+        }
+    }
+}
---
+++ Quests/VanillaQuests/Book1/Caving/Hell/GetHellstoneGear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestBooks.Quests.VanillaQuests.Book1.Caving.Hell
+{
+    internal class GetHellstoneGear : Quest
+    {
+        public override bool CheckCompletion()
+        {
+            return false; // Craft any item using Hellstone Bars
+        }
+    }
+}
