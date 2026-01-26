e9262f476398f32e1c713a570ca99dadc6a447f4
Blast <oshri040406@gmail.com>
2026-01-26 16:44:57 +0200

    - the rest of book 2 skeletons

---
+++ Quests/VanillaQuests/Book2/3MechtoGolem/GetChlorophyteBars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestBooks.Quests.VanillaQuests.Book2._3MechtoGolem
+{
+    internal class GetChlorophyteBars : Quest
+    {
+        public override bool CheckCompletion()
+        {
+            return false; // get X chlorophyte bars.
+                          // X should be a decently sizable amount, as chlorophyte on its own has hefty usage
+                          // shouldnt require crafting anything with it thise time,
+                          // as theres 3+ quests connected to this one where Chlorophyte is a material anyways
+        }
+    }
+}
---
+++ Quests/VanillaQuests/Book2/3MechtoGolem/PostGolemExtras/AlertMartiansProbe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestBooks.Quests.VanillaQuests.Book2._3MechtoGolem.PostGolemExtras
+{
+    internal class AlertMartiansProbe : Quest
+    {
+        public override bool CheckCompletion()
+        {
+            return false; // Start the martian madness / alert a martian probe
+        }
+    }
+}
