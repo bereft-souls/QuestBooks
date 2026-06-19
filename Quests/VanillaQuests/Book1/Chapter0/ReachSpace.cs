ef9eaf458c9aadf6e58d350adf7bfa9869d5fd49
shnakamura <joaovtnakamura@gmail.com>
2026-06-19 11:32:29 -0300

    feat: implements reach space quest

---
+++ Quests/VanillaQuests/Book1/Chapter0/ReachSpace.cs
@@ -0,0 +1,9 @@
+﻿using Terraria;
+
+namespace QuestBooks.Quests.VanillaQuests.Book1.Chapter0;
+
+public class ReachSpace : QBQuest
+{
+    // Hardcoded threshold adapted directly from Player::BordersMovement().
+    public override bool CheckCompletion() => Main.LocalPlayer.position.Y <= Main.topWorld + 640f + 16f;
+}
