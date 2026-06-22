adab5044324c8e5c176fbd65e3525d25f5811e02
NycroV <83246959+NycroV@users.noreply.github.com>
2026-06-22 15:33:57 -0500

    Move IO utils

--- Core/IO/TagCompoundUtilities.cs
+++
@@ -1,8 +0,0 @@
-﻿using Terraria.ModLoader.IO;
-
-namespace QuestBooks.Core.IO;
-
-public static class TagCompoundExtensions
-{
-    public static bool[] GetBoolArray(this TagCompound tag, string key) => tag.Get<bool[]>(key);
-}
---
+++ Utilities/IOUtils.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ModLoader.IO;
+
+namespace QuestBooks.Utilities
+{
+    public static partial class Utils
+    {
+        public static bool[] GetBoolArray(this TagCompound tag, string key) => tag.Get<bool[]>(key);
+    }
+}
