470eb1e337ce4edd0e4375a1ff0d160e5c6c02fa
shnakamura <joaovtnakamura@gmail.com>
2026-06-23 20:54:23 -0300

    fix: removes redundant operation on reach space

--- Quests/VanillaQuests/Book1/Chapter0/ReachSpace.cs
+++ Quests/VanillaQuests/Book1/Chapter0/ReachSpace.cs
@@ -3,5 +3,5 @@
 public class ReachSpace : QBQuest
 {
     // Hardcoded threshold adapted directly from Player::BordersMovement().
-    public override bool CheckCompletion() => Main.LocalPlayer.position.Y <= Main.topWorld + 640f + 16f;
+    public override bool CheckCompletion() => Main.LocalPlayer.position.Y <= Main.topWorld + 656f;
 }
