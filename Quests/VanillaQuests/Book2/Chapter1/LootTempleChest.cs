7c68ea0e6b16f55f43e85766e88841929626eff3
shnakamura <joaovtnakamura@gmail.com>
2026-06-23 20:52:13 -0300

    fix: makes loot temple chest use quest hooks

--- Quests/VanillaQuests/Book2/Chapter1/LootTempleChest.cs
+++ Quests/VanillaQuests/Book2/Chapter1/LootTempleChest.cs
@@ -7,19 +7,5 @@ public class LootTempleChest : QBQuest
 {
     public override bool CheckCompletion() => false;
 
-    public class LootTempleChestCheck : GlobalTile
-    {
-        public override void RightClick(int i, int j, int type)
-        {
-            if (!ChestSystem.IsNatural(i, j) || ChestSystem.IsExplored(i, j))
-                return;
-
-            var tile = Framing.GetTileSafely(i, j);
-
-            if (tile.TileFrameX != 16 * 36)
-                return;
-
-            QuestManager.MarkComplete<LootTempleChest>();
-        }
-    }
+    public class LootTempleChestCheck() : LootChestHook<LootTempleChest>(ChestFrames.Lihzahrd);
 }
