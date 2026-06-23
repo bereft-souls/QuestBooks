284ebdb816e5e245245542a4aa10fa54c3b8c88e
shnakamura <joaovtnakamura@gmail.com>
2026-06-23 20:53:14 -0300

    fix: removes duplicate craft hallowed pickaxe

--- Quests/VanillaQuests/Book2/Chapter1/CraftHallowedPick.cs
+++ Quests/VanillaQuests/Book2/Chapter1/CraftHallowedPick.cs
@@ -4,18 +4,18 @@ using Terraria.DataStructures;
 
 namespace QuestBooks.Quests.VanillaQuests.Book2.Chapter1;
 
-public class CraftHallowedPick : QBQuest
+public class CraftHallowedPickaxe : QBQuest
 {
     public override bool CheckCompletion() => false;
 
-    public class CraftHallowedPickCheck() : CraftItemHook(Complete)
+    public class CraftHallowedPickaxeCheck() : CraftItemHook(Complete)
     {
         private static void Complete(Item item, RecipeItemCreationContext context)
         {
             if (item.pick < 0 || !context.Recipe.HasIngredient(ItemID.HallowedBar))
                 return;
 
-            QuestManager.MarkComplete<CraftHallowedPick>();
+            QuestManager.MarkComplete<CraftHallowedPickaxe>();
         }
     }
 }
