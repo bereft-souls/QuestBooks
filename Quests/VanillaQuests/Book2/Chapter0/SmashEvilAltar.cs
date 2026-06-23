c96f15d352e08476cf9fd97c151c551d78d288b8
shnakamura <joaovtnakamura@gmail.com>
2026-06-23 20:50:49 -0300

    fix: makes smash evil altar use quest hooks

--- Quests/VanillaQuests/Book2/Chapter0/SmashEvilAltar.cs
+++ Quests/VanillaQuests/Book2/Chapter0/SmashEvilAltar.cs
@@ -1,4 +1,5 @@
-﻿using QuestBooks.Systems;
+﻿using QuestBooks.Quests.QuestSystems;
+using QuestBooks.Systems;
 
 namespace QuestBooks.Quests.VanillaQuests.Book2.Chapter0;
 
@@ -6,14 +7,5 @@ public class SmashEvilAltar : QBQuest
 {
     public override bool CheckCompletion() => false;
 
-    public class SmashEvilAltarCheck : GlobalTile
-    {
-        public override void KillTile(int i, int j, int type, ref bool fail, ref bool effectOnly, ref bool noItem)
-        {
-            if (type != TileID.DemonAltar)
-                return;
-
-            QuestManager.MarkComplete<SmashEvilAltar>();
-        }
-    }
+    public class SmashEvilAltarCheck() : KillTileHook<SmashEvilAltar>(TileID.DemonAltar);
 }
