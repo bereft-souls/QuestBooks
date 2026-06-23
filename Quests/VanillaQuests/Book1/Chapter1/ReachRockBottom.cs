664ab6890a390b1a7aafe3dc775081071469957b
shnakamura <joaovtnakamura@gmail.com>
2026-06-23 20:54:06 -0300

    fix: removes redundant operation on reach rock bottom

--- Quests/VanillaQuests/Book1/Chapter1/ReachRockBottom.cs
+++ Quests/VanillaQuests/Book1/Chapter1/ReachRockBottom.cs
@@ -3,5 +3,5 @@
 public class ReachRockBottom : QBQuest
 {
     // Hardcoded threshold adapted directly from Player::BordersMovement().
-    public override bool CheckCompletion() => Main.LocalPlayer.position.Y >= Main.bottomWorld - 640f - 32f - Main.LocalPlayer.height;
+    public override bool CheckCompletion() => Main.LocalPlayer.position.Y >= Main.bottomWorld - 672f - Main.LocalPlayer.height;
 }
