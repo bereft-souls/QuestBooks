04aff89a0f3621d0a59eead2c407ce4231c38285
shnakamura <joaovtnakamura@gmail.com>
2026-06-23 20:36:15 -0300

    fix: ensure quest check occurs before achievement

--- Quests/VanillaQuests/Book3/Chapter3/SurviveLongFall.cs
+++ Quests/VanillaQuests/Book3/Chapter3/SurviveLongFall.cs
@@ -7,17 +7,17 @@ public class SurviveLongFall : QBQuest
 {
     public override QuestType QuestType => QuestType.Player;
 
-    public override void Load() => On_AchievementsHelper.HandleSpecialEvent += On_AchievementsHelperOnHandleSpecialEvent;
+    public override void Load() => On_AchievementsHelper.HandleSpecialEvent += Check;
 
     public override bool CheckCompletion() => false;
 
-    private void On_AchievementsHelperOnHandleSpecialEvent(On_AchievementsHelper.orig_HandleSpecialEvent orig, Player player, int eventId)
+    private static void Check(On_AchievementsHelper.orig_HandleSpecialEvent orig, Player player, int eventId)
     {
+        if (eventId == AchievementHelperID.Special.SurviveHugeFall)
+        {
+            QuestManager.MarkComplete<SurviveLongFall>();
+        }
+        
         orig(player, eventId);
-
-        if (eventId != AchievementHelperID.Special.SurviveHugeFall)
-            return;
-
-        QuestManager.MarkComplete<SurviveLongFall>();
     }
 }
