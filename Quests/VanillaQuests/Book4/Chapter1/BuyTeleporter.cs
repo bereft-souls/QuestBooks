5f21910e5b30db6af2a2cb0d1a58603fb430ccd9
NycroV <83246959+NycroV@users.noreply.github.com>
2026-06-23 21:14:39 -0500

    Fix teleporterquest

--- Quests/VanillaQuests/Book4/Chapter1/BuyTeleporter.cs
+++ Quests/VanillaQuests/Book4/Chapter1/BuyTeleporter.cs
@@ -1,8 +1,17 @@
-﻿namespace QuestBooks.Quests.VanillaQuests.Book4.Chapter1;
+﻿using QuestBooks.Quests.QuestSystems;
+using QuestBooks.Systems;
+using Terraria.ModLoader.IO;
+
+namespace QuestBooks.Quests.VanillaQuests.Book4.Chapter1;
 
 public class BuyTeleporter : QBQuest
 {
+    public int TeleportersBought { get; set; }
+
     public override bool CheckCompletion() => false;
 
-    public class BuyTeleporterCheck() : BuyItemAmountHook<BuyTeleporter>(ItemID.Teleporter, 2);
+    public override void SaveProgress(TagCompound tag) => tag[nameof(TeleportersBought)] = TeleportersBought >= 2 ? null : TeleportersBought;
+    public override void LoadProgress(TagCompound tag) => TeleportersBought = tag.GetInt(nameof(TeleportersBought));
+
+    public class BuyTeleporterCheck() : BuyItemHook(item => Match(item, ItemID.Teleporter), static (_, _) => QuestManager.GetQuest<BuyTeleporter>().TeleportersBought++);
 }
