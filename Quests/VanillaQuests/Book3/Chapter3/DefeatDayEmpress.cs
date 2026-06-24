f8aff0091754c2cc1a9c15e61188df3f61e6a287
NycroV <83246959+NycroV@users.noreply.github.com>
2026-06-23 21:16:07 -0500

    Fix daytime EOL quest

--- Quests/VanillaQuests/Book3/Chapter3/DefeatDayEmpress.cs
+++ Quests/VanillaQuests/Book3/Chapter3/DefeatDayEmpress.cs
@@ -7,14 +7,12 @@ public class DefeatDayEmpress : QBQuest
 {
     public override bool CheckCompletion() => false;
 
-    public class DayEmpressOfLightCheck() : KillNPCHook<DefeatDayEmpress>(Complete)
+    public class DayEmpressOfLightCheck() : KillNPCHook(npc => Match(npc, NPCID.HallowBoss), CheckDaytimeEOL)
     {
-        private static new void Complete(NPC npc)
+        private static void CheckDaytimeEOL(NPC npc)
         {
             if (npc.type != NPCID.HallowBoss || !npc.AI_120_HallowBoss_IsGenuinelyEnraged())
-            {
                 return;
-            }
 
             QuestManager.MarkComplete<DefeatDayEmpress>();
         }
