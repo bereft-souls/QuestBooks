ae10865d88c8837a87ef214d66e6059fdd932dfc
shnakamura <joaovtnakamura@gmail.com>
2026-06-23 21:09:28 -0300

    fix: makes craft evil bar armor use quest hooks

--- Quests/VanillaQuests/Book1/Chapter0/CraftEvilBarArmor.cs
+++ Quests/VanillaQuests/Book1/Chapter0/CraftEvilBarArmor.cs
@@ -1,51 +1,78 @@
-﻿using Terraria.DataStructures;
+﻿using QuestBooks.Systems;
+using Terraria.DataStructures;
+using Terraria.ModLoader.IO;
 
 namespace QuestBooks.Quests.VanillaQuests.Book1.Chapter0;
 
 public class CraftEvilBarArmor : QBQuest
 {
+    private const string HeadTag = "CraftedEvilArmorHead";
+    
+    private const string BodyTag = "CraftedEvilArmorBody";
+    
+    private const string LegsTag = "CraftedEvilArmorLegs";
+    
+    /// <summary>
+    ///     Gets a value indicating whether the player has crafted the helmet piece of an armor set made
+    ///     from an evil bar.
+    /// </summary>
+    public bool Head { get; private set; }
+
+    /// <summary>
+    ///     Gets a value indicating whether the player has crafted the chestplate piece of an armor set
+    ///     made from an evil bar.
+    /// </summary>
+    public bool Body { get; private set; }
+
+    /// <summary>
+    ///     Gets a value indicating whether the player has crafted the legging piece of an armor set made
+    ///     from an evil bar.
+    /// </summary>
+    public bool Legs { get; private set; }
+    
     public override QuestType QuestType => QuestType.Player;
 
-    public override bool CheckCompletion() => CraftEvilBarArmorCheck.Complete;
+    public override bool CheckCompletion() => Head && Body && Legs;
+
+    public override void SaveProgress(TagCompound tag)
+    {
+        tag[HeadTag] = Head;
+        tag[BodyTag] = Body;
+        tag[LegsTag] = Legs;
+    }
+    
+    public override void LoadProgress(TagCompound tag)
+    {
+        Head = tag.GetBool(HeadTag);
+        Body = tag.GetBool(BodyTag);
+        Legs = tag.GetBool(LegsTag);
+    }
 
     public class CraftEvilBarArmorCheck : GlobalItem
     {
-        /// <summary>
-        ///     Gets a value indicating whether the player has crafted the helmet piece of an armor set made
-        ///     from an evil bar.
-        /// </summary>
-        public static bool Head { get; private set; }
-
-        /// <summary>
-        ///     Gets a value indicating whether the player has crafted the chestplate piece of an armor set
-        ///     made from an evil bar.
-        /// </summary>
-        public static bool Body { get; private set; }
-
-        /// <summary>
-        ///     Gets a value indicating whether the player has crafted the legging piece of an armor set made
-        ///     from an evil bar.
-        /// </summary>
-        public static bool Legs { get; private set; }
-
-        /// <summary>
-        ///     Gets a value indicating whether the player has crafted all three pieces of an armor set made
-        ///     from an evil bar.
-        /// </summary>
-        /// <value>
-        ///     <see langword="true"/> if the player has crafted all three pieces of an armor set made from an
-        ///     evil bar; otherwise, <see langword="false"/>.
-        /// </value>
-        public static bool Complete => Head && Body && Legs;
-
         public override void OnCreated(Item item, ItemCreationContext context)
         {
             if (context is not RecipeItemCreationContext)
+            {
                 return;
+            }
+            
+            var quest = QuestManager.GetQuest<CraftEvilBarArmor>();
 
-            Head |= item.type == ItemID.CrimsonHelmet || item.type == ItemID.ShadowHelmet;
-            Body |= item.type == ItemID.CrimsonScalemail || item.type == ItemID.ShadowScalemail;
-            Legs |= item.type == ItemID.CrimsonGreaves || item.type == ItemID.ShadowGreaves;
+            if (!quest.Head)
+            {
+                quest.Head |= item.type == ItemID.CrimsonHelmet || item.type == ItemID.ShadowHelmet;
+            }
+
+            if (!quest.Body)
+            {
+                quest.Body |= item.type == ItemID.CrimsonScalemail || item.type == ItemID.ShadowScalemail;
+            }
+
+            if (!quest.Legs)
+            {
+                quest.Legs |= item.type == ItemID.CrimsonGreaves || item.type == ItemID.ShadowGreaves;
+            }
         }
     }
 }
