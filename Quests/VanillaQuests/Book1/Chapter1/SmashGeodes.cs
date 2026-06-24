c9a36efc8a170d61e70b4d13a21745c99174beb3
shnakamura <joaovtnakamura@gmail.com>
2026-06-23 21:10:50 -0300

    fix: makes smash geodes use quest hooks

--- Quests/VanillaQuests/Book1/Chapter1/SmashGeodes.cs
+++ Quests/VanillaQuests/Book1/Chapter1/SmashGeodes.cs
@@ -1,40 +1,42 @@
-﻿using Terraria.ModLoader.IO;
+﻿using QuestBooks.Systems;
+using Terraria.ModLoader.IO;
 
 namespace QuestBooks.Quests.VanillaQuests.Book1.Chapter1;
 
 public class SmashGeodes : QBQuest
 {
-    public override bool CheckCompletion() => SmashGeodesCheck.Completed;
+    private const string Tag = "SmashedGeodesCount";
+    
+    /// <summary>
+    ///     The number of geodes the player must smash in order to complete the quest.
+    /// </summary>
+    public const int GeodesTarget = 10;
+
+    /// <summary>
+    ///     Gets the number of geodes the player has smashed.
+    /// </summary>
+    public int GeodesSmashed { get; private set; }
+    
+    public override bool CheckCompletion() => GeodesSmashed >= GeodesTarget;
+
+    public override void SaveProgress(TagCompound tag) => tag[Tag] = GeodesSmashed;
+    
+    public override void LoadProgress(TagCompound tag) => GeodesSmashed = tag.GetInt(Tag);
 
     public class SmashGeodesCheck : GlobalItem
     {
-        private const string Tag = "Count";
-
-        /// <summary>
-        ///     The number of geodes the player must smash in order to complete the quest.
-        /// </summary>
-        public const int Target = 10;
-
-        /// <summary>
-        ///     Gets the number of geodes the player has smashed.
-        /// </summary>
-        public static int Count { get; private set; }
-
-        /// <summary>
-        ///     Gets a value indicating whether the player has smashed enough geodes to complete the quest.
-        /// </summary>
-        /// <value>
-        ///     <see langword="true"/> if <see cref="Count"/> is greater than or equal to <see cref="Target"/>;
-        ///     otherwise, <see langword="false"/>.
-        /// </value>
-        public static bool Completed => Count >= Target;
-
         public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.Geode;
 
-        public override void RightClick(Item item, Player player) => Count++;
+        public override void RightClick(Item item, Player player)
+        {
+            var quest = QuestManager.GetQuest<SmashGeodes>();
 
-        public override void SaveData(Item item, TagCompound tag) => tag[Tag] = Count;
-
-        public override void LoadData(Item item, TagCompound tag) => Count = tag.GetInt(Tag);
+            if (quest.Completed)
+            {
+                return;
+            }
+            
+            quest.GeodesSmashed++;
+        }
     }
 }
